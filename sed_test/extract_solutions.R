db9a16cb58054b5d9965f306ef0ce4b564c47716
Darrin Speegle <speegled@slu.edu>
2021-02-07 16:52:03 -0600

    how to make solutions html

---
+++ sed_test/extract_solutions.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+library(stringr)
+
+ff <- list.files("./")
+ff <- ff[str_detect(ff, "Rmd")]
+ff <- ff[!str_detect(ff, "pre-chapter")]
+filename <- ff[2]
+for(filename in ff) {
+  cc <- paste0("./", filename)
+  dd <- paste(readLines(cc), collapse = "\n")
+  #if(str_detect(dd, "\\.solution")) {
+    writeLines(paste0("# Exercises Chapter ", as.integer(str_extract(filename, "[0-9]*")) + 1, "\n```{r child = \'pre-chapter-script.Rmd\'}\n```", str_remove_all(dd, "[\\S\\s]*?## Exercises"), collapse = ""), con = cc)
+  #}
+}
