1e758d1f149eb9369d8558e8e7745719646ad3e3
Darrin Speegle <speegled@slu.edu>
2021-02-17 15:54:03 -0600

    simpler sols

--- sed_test/extract_solutions.R
+++ sed_test/extract_solutions.R
@@ -9,7 +9,15 @@ filename <- ff[2]
 for(filename in ff) {
   cc <- paste0("./", filename)
   dd <- paste(readLines(cc), collapse = "\n")
+  dd <- paste0("# Exercises Chapter ", 
+               as.integer(str_extract(filename, "[0-9]*")) + 1, 
+               "\n```{r child = \'pre-chapter-script.Rmd\'}\n```", 
+               str_remove_all(dd, "[\\S\\s]*?## Exercises"), 
+               collapse = "")
+  
+  dd <- str_replace_all(dd, ":::\\s*\\{\\.solution #hide[\\S\\s]*?:::", "::: solution \n\n:::")
+  
   #if(str_detect(dd, "\\.solution")) {
-    writeLines(paste0("# Exercises Chapter ", as.integer(str_extract(filename, "[0-9]*")) + 1, "\n```{r child = \'pre-chapter-script.Rmd\'}\n```", str_remove_all(dd, "[\\S\\s]*?## Exercises"), collapse = ""), con = cc)
+    writeLines(dd, con = cc)
   #}
 }
